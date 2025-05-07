914f93398e20fd0cc0c6cac08af914f906300d74
Wei <62009886+jackywi2@users.noreply.github.com>
2025-05-07 17:30:46 +0800

    Add files via upload

---
+++ Solution/0021.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        if not list1 or not list2:
+            return list1 if list1 else list2
+
+        if list1.val > list2.val:
+            list1, list2 = list2, list1
+
+        list1.next = self.mergeTwoLists(list1.next, list2)
+        return list1
+        '''
+        # Use ListNode
+        head = ListNode()
+        current = head
+        while list1 and list2:
+            if list1.val < list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+            current = current.next
+
+        current.next = list1 or list2
+        return head.next
+        '''
