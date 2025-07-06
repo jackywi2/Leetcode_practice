044bb2d172502278e440fb8271653997160ba2cf
蔡昕瑋 <62009886+jackywi2@users.noreply.github.com>
2025-07-06 16:18:19 +0800

    Add files via upload

---
+++ Solution/0024.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        first_node = head
+        second_node = head.next
+
+        # Swapping
+        first_node.next = self.swapPairs(second_node.next) # let head jump to the next next
+        second_node.next = first_node
+
+        # Now the head is the second node
+        return second_node   # return head
+            
